f675a6f1e1ac0ba82d7771d44dd4d13d77c32433
Alvin21Bon <alvin21bon@gmail.com>
2023-02-01 07:39:39 -0500

    Added new feature of build-mod.bat: store Quake mod folders into cache for persistent memory to use for deletion of folders

--- mod-build/build-mod.bat
+++ mod-build/build-mod.bat
@@ -2,9 +2,22 @@
 
 set mod-target=D:\program-files\Steam\steamapps\common\Quake 4\my-file
 
-for /D %%a in (./mod-files/*) do (
-    echo moving "./mod-files/%%a" to "%mod-target%"
+:: delete every folder named in folder-cache 
+echo ---DELETING FOLDERS IN QUAKE MOD FOLDER---
+for /F "tokens=*" %%b in (folder-cache.txt) do (
+    echo deleting "%%b" ...
+    rmdir "%mod-target%/%%b" /Q /S 1>nul
+)
+echo:
 
-    rm -r "%mod-target%/%%a"  2> nul
+:: reset folder-cache 
+break>folder-cache.txt
+echo ---TRANSFERRING FILES FROM ./mod-files TO QUAKE MOD FOLDER---
+for /D %%a in (./mod-files/*) do (
+    echo moving "./mod-files/%%a"
+    echo %%a>> folder-cache.txt
+
     Xcopy "./mod-files/%%a" "%mod-target%/%%a" /E/Y/I > nul
 )
+
+pause

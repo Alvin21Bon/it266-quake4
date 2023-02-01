e9f5a051ed0eff8902055076c57743b2a56dbac4
Alvin21Bon <alvin21bon@gmail.com>
2023-02-01 02:24:33 -0500

    Added new batch file to automatically transfer all edited files (GUIS, .def files, etc.) into the Quake mod folder.

--- mod-build/build-mod.bat
+++ mod-build/build-mod.bat
@@ -1,5 +1,10 @@
 @echo off 
 
-echo awesome sauce
+set mod-target=D:\program-files\Steam\steamapps\common\Quake 4\my-file
 
-pause
+for /D %%a in (./mod-files/*) do (
+    echo moving "./mod-files/%%a" to "%mod-target%"
+
+    rm -r "%mod-target%/%%a"  2> nul
+    Xcopy "./mod-files/%%a" "%mod-target%/%%a" /E/Y/I > nul
+)
